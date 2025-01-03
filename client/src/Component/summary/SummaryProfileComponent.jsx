0310a7379678813f5f163f58f4739c21e9d1e981
md enamul <hfiroz1998@gmail.com>
2025-01-03 21:05:58 +0600

    change pc

--- client/src/Component/summary/SummaryProfileComponent.jsx
+++ client/src/Component/summary/SummaryProfileComponent.jsx
@@ -3,12 +3,12 @@
 const SummaryProfileComponent = () => {
     return (
         <>
-            <div className="mx-2 mt-2  rounded border-2 border-gray-100">
+            <div className="mx-2 mt-2  rounded border-2 border-gray-100 overflow-hidden">
 
                 <div className="h-[100px] w-full overflow-hidden flex flex-row justify-between items-center ">
                     <img src="/image/coverPhoto.jpeg" alt="Cover Photo" className="min-w-full min-h-full" />
                 </div>
-                <div className="h-[70px] w-[70px] rounded-full overflow-hidden flex flex-row justify-between items-center ms-[20px] mt-[-35px] ">
+                <div className="h-[70px] w-[70px] rounded-full overflow-hidden flex flex-row justify-between items-center ms-[10px] mt-[-35px] ">
                     <img src="/image/coverPhoto.jpeg" alt="Cover Photo" className="min-w-full min-h-full"/>
                 </div>
 
