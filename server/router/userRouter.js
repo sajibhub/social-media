fd6a8456853807c77145fcdb3a9bb7d99eb12b6f
sajib-bd <mohammadsajib996@gmail.com>
2025-01-05 22:25:38 +0600

    fix userRouter not export file import remove

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -7,7 +7,6 @@ import {
   Profile,
   ForgetOTP,
   PasswordReset,
-  profileUpdate,
 } from "../controllers/userController.js";
 import Authorized from "../middleware/authorized.js";
 
@@ -19,6 +18,5 @@ userRouter.post("/user/auth/logout", Authorized, Logout);
 userRouter.get("/user/profile", Authorized, Profile);
 userRouter.post("/user/auth/forger/password/:email", ForgetOTP);
 userRouter.put("/user/auth/forger/password", PasswordReset);
-userRouter.put("/user/profile/profile", Authorized, profileUpdate);
 
 export default userRouter;
